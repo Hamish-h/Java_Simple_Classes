6c77ba642eb1c56a11212b75e8c4778a0c259ca6
Hanish-H <hamish.h@outlook.com>
2018-05-14 22:48:05 +0100

    moved test files to test folder

--- src/test/java/Water_bottle_test.java
+++ src/test/java/Water_bottle_test.java
@@ -1,34 +1,32 @@
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Water_bottle_test {
 
     @Test
-    public void volume(){
+    public void volume() {
         Water_bottle volume = new Water_bottle();
         assertEquals(100, volume.getVolume());
     }
 
     @Test
-    public void drink(){
+    public void drink() {
         Water_bottle volume = new Water_bottle();
         volume.drink();
         assertEquals(90, volume.getVolume());
     }
 
     @Test
-    public void fill(){
+    public void fill() {
         Water_bottle volume = new Water_bottle();
         volume.fill();
         assertEquals(100, volume.getVolume());
     }
 
     @Test
-    public void empty(){
+    public void empty() {
         Water_bottle volume = new Water_bottle();
         volume.empty();
         assertEquals(0, volume.getVolume());
     }
 }
-
